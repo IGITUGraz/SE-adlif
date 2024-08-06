a674c53a06886f880aec49dd53ae0513ecde32ec
rferrand <romain.ferrand@tugraz.at>
2024-08-06 12:46:23 +0200

    add and test lif,li,adlif models, change notation to fit paper

--- models/lif.py
+++ models/lif.py
@@ -3,7 +3,8 @@ from typing import Optional
 from models.helpers import SLAYER
 import torch
 import torch.nn.functional as F
-from torch import Module, Tensor
+from torch.nn import Module
+from torch import Tensor
 from torch.nn.parameter import Parameter
 
 from module.tau_trainers import TauTrainer, get_tau_trainer_class
@@ -20,7 +21,7 @@ from module.tau_trainers import TauTrainer, get_tau_trainer_class
 # hardcoded slayer as "gradient injection"
 
 
-class LIFLayer(Module):
+class LIF(Module):
     __constants__ = ["in_features", "out_features"]
     in_features: int
     out_features: int
@@ -30,12 +31,12 @@ class LIFLayer(Module):
         self,
         in_features: int,
         out_features: int,
-        thr: float = 1.0,
-        tau_u: tuple[float, float] = (20, 20),
-        train_tau_u_method: str = "fixed",
         use_recurrent: bool = True,
+        thr: float = 1.0,
         alpha: float = 5.0,
         c: float = 0.4,
+        tau_u_range: tuple[float, float] = (20, 20),
+        train_tau_u_method: str = "fixed",
         device=None,
         dtype=None,
         **kwargs,
@@ -44,8 +45,8 @@ class LIFLayer(Module):
         super().__init__(**kwargs)
         self.in_features = in_features
         self.out_features = out_features
-        self.dt = 1
-        self.tau_u_range = tau_u
+        self.dt = 1.0
+        self.tau_u_range = tau_u_range
         self.use_recurrent = use_recurrent
         self.thr = thr
         self.alpha = alpha
@@ -59,6 +60,8 @@ class LIFLayer(Module):
             self.recurrent = Parameter(
                     torch.empty((out_features, out_features), **factory_kwargs)
                 )
+        else:
+            self.register_buffer("recurrent", None)
         self.tau_u_trainer: TauTrainer = get_tau_trainer_class(train_tau_u_method)(
             out_features,
             self.dt,
@@ -69,6 +72,7 @@ class LIFLayer(Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        self.tau_u_trainer.reset_parameters()
         torch.nn.init.uniform_(
             self.weight,
             -1.0 * torch.sqrt(1 / torch.tensor(self.in_features)),
@@ -83,44 +87,44 @@ class LIFLayer(Module):
 
     def initial_state(
         self, batch_size: int, device: Optional[torch.device] = None
-    ) -> torch.Tensor:
+    ) -> Tensor:
         size = (batch_size, self.out_features)
-        vs = torch.zeros(
+        u = torch.zeros(
             size=size, device=device, dtype=torch.float, requires_grad=True
         )
         z = torch.zeros(size=size, device=device, dtype=torch.float, requires_grad=True)
-        return vs, z
+        return u, z
 
-    def forward(self, input_tensor: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, input_tensor: Tensor) -> tuple[Tensor, Tensor]:
         
-        v0, z0 = self.initial_state(input_tensor.size(0), input_tensor.device)
-        v = [v0,]
+        u0, z0 = self.initial_state(input_tensor.size(0), input_tensor.device)
+        u = [u0,]
         z = [z0,]
         outputs = []
-        decay_vs = self.tau_trainer.get_decay()
+        decay_u = self.tau_u_trainer.get_decay()
         for i in range(input_tensor.size(1)):
-            v_tm1 = v[-1]
+            u_tm1 = u[-1]
             z_tm1 = z[-1]
-            v_tm1 = v_tm1 * (1 - z_tm1.detach())
+            u_tm1 = u_tm1 * (1 - z_tm1.detach())
 
             soma_current = F.linear(input_tensor[:, i], self.weight, self.bias)
             if self.use_recurrent:
                 soma_rec_current = F.linear(z_tm1, self.recurrent, None)
-            soma_current += soma_rec_current
+                soma_current += soma_rec_current
 
-            v_t = decay_vs * v_tm1 + (1.0 - decay_vs) * (soma_current)
+            u_t = decay_u * u_tm1 + (1.0 - decay_u) * (soma_current)
 
-            v_thr = v_t - self.thr
+            u_thr = u_t - self.thr
             # Forward Gradient Injection trick (credits to Sebastian Otte)
-            z_t = torch.heaviside(v_thr, 1.0).detach() + (v_thr - v_thr.detach()) * SLAYER(v_thr, self.alpha, self.c).detach()
+            z_t = torch.heaviside(u_thr, torch.as_tensor(0.0).type(u_thr.dtype)).detach() + (u_thr - u_thr.detach()) * SLAYER(u_thr, self.alpha, self.c).detach()
 
             outputs.append(z_t)
-            v.append(v_t)
+            u.append(u_t)
             z.append(z_t)
         
-        v = torch.stack(v, dim=1)
+        u = torch.stack(u, dim=1)
         z = torch.stack(z, dim=1)
-        states = torch.stack([v, z], dim=0)
+        states = torch.stack([u, z], dim=0)
         outputs = torch.stack(outputs, dim=1)
         return outputs, states
 
